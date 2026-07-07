15bbd4af3b4cc65274f048cdcb1dd38f25577431
Riya Rathore <riyarathore1825@gmail.com>
2026-07-07 09:09:14 +0530

    Time: 1 ms (99.85%), Space: 42.3 MB (96.71%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -2,10 +2,13 @@ class Solution {
     public long sumAndMultiply(int n) {
         long x = 0;
         long s = 0;
+
         for(char c : String.valueOf(n).toCharArray())
-        if(c != '0'){
-            x = x *10+ c-'0';
-            s += c-'0';
+        {
+            if(c != '0'){
+                x = x*10+c-'0';
+                s += c-'0';
+            }
         }
         return x*s;
     }
